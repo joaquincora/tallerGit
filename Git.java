519ba1ecd137d00314406717f8ccb3979daa5b59
joaquín Coronado Ramírez <joaquincora@hotmail.com>
2017-05-28 13:39:34 -0500

    Segundo Commit

--- Git.java
+++ Git.java
@@ -5,5 +5,8 @@ public class Git{
 			System.out.println("hola mundo");
 			System.out.println("Mesaje del primer commit");
 
+			System.out.println("el proximo GuruDay será de Hacking");
+			System.out.println("Mensaje para el segundo commit");
+
 	}
 }
