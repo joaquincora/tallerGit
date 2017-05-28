e06544a4e2735dd5bb64c4892d281540da865a07
joaquín Coronado Ramírez <joaquincora@hotmail.com>
2017-05-28 13:44:55 -0500

    Tercer Commit

--- Git.java
+++ Git.java
@@ -2,11 +2,14 @@ public class Git{
 	
 	public static void main(String arg[]){
 
-			System.out.println("hola mundo");
+			System.out.println("Hola mundo");
 			System.out.println("Mesaje del primer commit");
 
-			System.out.println("el proximo GuruDay será de Hacking");
+			System.out.println("El proximo GuruDay será de Hacking");
 			System.out.println("Mensaje para el segundo commit");
 
+
+			System.out.println("Los alumnos que esten inscritos a algun diplomado de TecGurus no pagan el GuruDay");
+			System.out.println("Mensaje del tercer commit");
 	}
 }
